93f4b01847bbd3579fa9b31a502a309caf9ee33e
Angela <tatazinhagms2@gmail.com>
2023-09-15 18:18:47 -0300

    Projected created

---
+++ src/test/java/com/dev/Java/Spring/JavaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.Java.Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
